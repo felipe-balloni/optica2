82d8a8614874c85c01b79b6ca3f4f95a0c4e7239
Felipe Balloni Ferreira <felipe.balloni@fbftecnologia.com.br>
2021-12-10 19:06:41 -0300

    Packages installation: Filament and Spatie/Permission e configurations

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+        "./vendor/filament/**/*.blade.php",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                danger: colors.rose,
+                primary: colors.blue,
+                success: colors.green,
+                warning: colors.yellow,
+            },
+        },
+    },
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography')
+    ],
+}
