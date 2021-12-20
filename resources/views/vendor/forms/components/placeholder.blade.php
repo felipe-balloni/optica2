6b7ed217750cb0a1b67bf9a28f9de645d3cf9fba
Felipe Balloni Ferreira <felipe.balloni@hotmail.com>
2021-12-20 19:18:51 -0300

    Publish resources to customize

---
+++ resources/views/vendor/forms/components/placeholder.blade.php
@@ -0,0 +1,10 @@
+<x-forms::field-wrapper
+    :id="$getId()"
+    :label="$getLabel()"
+    :label-sr-only="$isLabelHidden()"
+    :helper-text="$getHelperText()"
+    :hint="$getHint()"
+    :state-path="$getStatePath()"
+>
+    {{ $getContent() }}
+</x-forms::field-wrapper>
